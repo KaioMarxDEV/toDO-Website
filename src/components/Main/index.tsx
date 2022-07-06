ce1ba2b14650d6e2724ecf212a924cb5d5ac3b9d
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-06 06:33:31 -0300

    feat: style of tasks header

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,29 @@
+import { PlusCircle } from 'phosphor-react'
 import { Input } from '../Input'
 import styles from './main.module.css'
 
 export function Main() {
   return (
     <div className={styles.main}>
-      <Input handleInputChange={() => { }} />
-      <button>
-        <span>Criar</span>
-        <PlusCircle size={32} />
-      </button>
+      <div className={styles.inputSection}>
+        <Input handleInputChange={() => { }} />
+        <button className={styles.createTask} >
+          <span>Create</span>
+          <PlusCircle size={16} />
+        </button>
+      </div>
+      <div className={styles.tasks}>
+        <header className={styles.header}>
+          <div>
+            <span className={styles.headerBlue}>Tasks Created</span>
+            <span className={styles.createdRatio}>5</span>
+          </div>
+          <div>
+            <span className={styles.headerPurple}>Tasks Completed</span>
+            <span className={styles.completedRatio}>2 de 5</span>
+          </div>
+        </header>
+      </div>
     </div>
   )
 }
